8657af97bd0cb83bfef88ad2750f412f6deffc48
ndrewantonio <dev.andrewantonio@gmail.com>
2025-12-03 01:12:01 +0700

    feat: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,36 @@
+import Button from '@/ui/Button'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <div className="w-[488px] text-center font-normal md:w-[600px]">
+        <video
+          width="100%"
+          height="auto"
+          loop
+          muted
+          autoPlay
+          playsInline
+          preload="none"
+          style={{ objectFit: 'cover' }}
+        >
+          <source src={'/assets/404.mp4'} type="video/mp4" />
+        </video>
+
+        <div className="mb-4 grid md:mb-6">
+          <p className="mb-2 text-2xl font-medium md:text-3xl">
+            This page is lost in the wild
+          </p>
+          <p className="text-sm md:text-base lg:text-lg">
+            We couldn&apos;t find the page you were looking for.
+          </p>
+        </div>
+
+        <Link href="/">
+          <Button>Go back home</Button>
+        </Link>
+      </div>
+    </main>
+  )
+}
