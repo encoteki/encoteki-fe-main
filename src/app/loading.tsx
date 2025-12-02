609a81b43313c991bd4d00fa4e82af526365da42
ndrewantonio <dev.andrewantonio@gmail.com>
2025-12-03 01:10:45 +0700

    feat: add global loading state

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Tiggy from '@/assets/tsb/Tiggy_tp.webp'
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-(--khaki-90)">
+      <div className="space-y-3">
+        <Image className="animate-spin" alt="Loading" src={Tiggy} />
+      </div>
+    </div>
+  )
+}
