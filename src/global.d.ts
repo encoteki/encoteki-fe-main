07ddd0ca7fea22b910110634d4c413af9899ffdd
ndrewantonio <dev.andrewantonio@gmail.com>
2025-12-03 01:09:07 +0700

    chore: declare global types

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.webp' {
+  const value: string
+  export default value
+}
+
+// tell TS that any .css file is a valid module
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+declare module '*.mp4'
