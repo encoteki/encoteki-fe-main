59fb26d6a63a70484026da3948cd8f75084d41eb
ndrewantonio <dev.andrewantonio@gmail.com>
2026-02-06 06:43:01 +0700

    chore: convert configuration files to ES modules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,16 @@
-const path = require('path')
+import path from 'path'
 
+/**
+ * Helper to build the ESLint command for lint-staged
+ * @param {string[]} filenames
+ */
 const buildEslintCommand = (filenames) =>
   `eslint --fix ${filenames
     .map((f) => `"${path.relative(process.cwd(), f)}"`)
     .join(' ')}`
 
-module.exports = {
+const config = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 }
+
+export default config
